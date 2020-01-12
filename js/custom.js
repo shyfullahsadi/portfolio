1e15cf0259acdbfd04d86db8955a46fe1bbb6584
shyfullahsadi <shyfullahsadi@outlook.com>
2020-01-12 18:02:40 +0600

    Upto Service Part without responsive

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(function(){
+    // Typed JS
+
+    $(".typed").typed({
+		strings: ["Web Design.", "Web Development.", "UI/UX Design."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 100,
+		// time before typing starts
+		startDelay: 200,
+		// backspacing speed
+		backSpeed: 50,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+
+
+})
